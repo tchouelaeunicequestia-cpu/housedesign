125396c9882d3d46601443b91bf4bbe8c7e03cde
Tchouela eunice <tchouelaeunice.questia@ictuniversity.edu.cm>
2026-07-13 17:13:13 +0100

    Update controller to use DTO

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Post, Body, Get, Param } from '@nestjs/common';
 import { ProjectService } from './project.service';
+import { CreateProjectDto } from './dto/create-project.dto';
 
 @Controller('projects')
 export class ProjectController {
   constructor(private readonly projectService: ProjectService) {}
 
   @Post()
-  create(@Body() body: any) {
-    return this.projectService.create(body);
+  create(@Body() createProjectDto: CreateProjectDto) {
+    return this.projectService.create(createProjectDto);
   }
 
   @Get()
