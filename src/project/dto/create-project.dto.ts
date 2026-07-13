d05e41c94a46aa81c702cad5c38dde16c0f9c992
Tchouela eunice <tchouelaeunice.questia@ictuniversity.edu.cm>
2026-07-13 17:10:53 +0100

    Create dto folder and file

---
+++ src/project/dto/create-project.dto.ts
@@ -0,0 +1,15 @@
+import { IsString, IsNotEmpty, IsOptional, IsUrl } from 'class-validator';
+
+export class CreateProjectDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsOptional()
+  @IsUrl()
+  imageUrl?: string;
+}
